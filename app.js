c7aa0c228efc27b183e1901534af28a961617bab
Fl0w <52174534+LPSebi@users.noreply.github.com>
2024-04-06 23:50:30 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+
+const app = express()
+
+const urls = []
+
+app.post('api/shorturl', (req, res) => {
+    urls.push(req.body.url)
+    if (
+        !(
+            req.body.url.startsWith('http://') ||
+            req.body.url.startsWith('https://')
+        )
+    ) {
+        res.json({
+            error: 'invalid url',
+        })
+    }
+    res.json({
+        original_url: 'https://www.google.com',
+        short_url: urls.length - 1,
+    })
+})
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+    const short_url = req.params.short_url
+    res.redirect(urls[short_url])
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
