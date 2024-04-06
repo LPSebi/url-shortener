2784e911b5b05ea88c0ac811074480829c3f4a57
Fl0w <52174534+LPSebi@users.noreply.github.com>
2024-04-07 00:14:55 +0200

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import dns from 'dns'
 const app = express()
-app.use(bodyParser.text())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const urls = []
 
